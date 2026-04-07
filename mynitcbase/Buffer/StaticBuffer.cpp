e7e158367347898ab394e87e5d025b4e1cee7a63
Devadath <devadathkartha@gmail.com>
2026-04-07 15:24:37 +0000

    stage6

--- mynitcbase/Buffer/StaticBuffer.cpp
+++ mynitcbase/Buffer/StaticBuffer.cpp
@@ -4,41 +4,82 @@ unsigned char StaticBuffer::blocks[BUFFER_CAPACITY][BLOCK_SIZE];
 struct BufferMetaInfo StaticBuffer::metainfo[BUFFER_CAPACITY];
 
 StaticBuffer::StaticBuffer() {
-  // Initialize all blocks as free
-  for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++) {
-    metainfo[bufferIndex].free = true;
-  }
+
+    // initialise all buffer slots as free and clean
+    for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++) {
+        metainfo[bufferIndex].free      = true;
+        metainfo[bufferIndex].dirty     = false;
+        metainfo[bufferIndex].timeStamp = -1;
+        metainfo[bufferIndex].blockNum  = -1;
+    }
 }
 
 // The destructor is empty for now. 
 // In Stage 4, we will add code here to save changes back to the disk when we exit!
-StaticBuffer::~StaticBuffer() {}
+StaticBuffer::~StaticBuffer() {
+
+    // iterate through all buffer slots
+    for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++) {
+
+        // only care about occupied AND dirty buffers
+        if (metainfo[bufferIndex].free == false && 
+            metainfo[bufferIndex].dirty == true) {
+
+            // write the buffer contents back to the correct disk block
+            Disk::writeBlock(blocks[bufferIndex], metainfo[bufferIndex].blockNum);
+        }
+    }
+}
 
 int StaticBuffer::getFreeBuffer(int blockNum) {
-  // DISK_BLOCKS is 8192 (0 to 8191)
-  if (blockNum < 0 || blockNum >= DISK_BLOCKS) {
-    return E_OUTOFBOUND;
-  }
-  
-  int allocatedBuffer = -1;
 
-  // 2. Iterate through the clipboard to find a free slot
-  for (int i = 0; i < BUFFER_CAPACITY; i++) {
-      if (metainfo[i].free == true) {
-          allocatedBuffer = i;
-          break; // Found one! Stop searching.
-      }
-  }
+    // Step 1: validate blockNum
+    if (blockNum < 0 || blockNum >= DISK_BLOCKS) {
+        return E_OUTOFBOUND;
+    }
 
-  // Note: For this stage, we assume there is ALWAYS a free buffer.
-  // (In later stages, if the buffer is full, we will have to "evict" an old block).
+    // Step 2: increment timestamp of ALL occupied buffer slots
+    for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++) {
+        if (metainfo[bufferIndex].free == false) {
+            metainfo[bufferIndex].timeStamp++;
+        }
+    }
 
-  // 3. Update the clipboard to say this slot is now taken
-  metainfo[allocatedBuffer].free = false;
-  metainfo[allocatedBuffer].blockNum = blockNum;
+    // Step 3: look for a free buffer slot
+    int bufferNum = -1;
 
-  // 4. Return the index of the slot (e.g., Slot 0, 1, 2...)
-  return allocatedBuffer;
+    for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++) {
+        if (metainfo[bufferIndex].free == true) {
+            bufferNum = bufferIndex;
+            break;
+        }
+    }
+
+    // Step 4: if no free slot, find the LRU buffer (highest timestamp)
+    if (bufferNum == -1) {
+
+        int maxTimeStamp = -1;
+
+        for (int bufferIndex = 0; bufferIndex < BUFFER_CAPACITY; bufferIndex++) {
+            if (metainfo[bufferIndex].timeStamp > maxTimeStamp) {
+                maxTimeStamp = metainfo[bufferIndex].timeStamp;
+                bufferNum = bufferIndex;
+            }
+        }
+
+        // Step 5: if the LRU buffer is dirty, write it back to disk
+        if (metainfo[bufferNum].dirty == true) {
+            Disk::writeBlock(blocks[bufferNum], metainfo[bufferNum].blockNum);
+        }
+    }
+
+    // Step 6: update metainfo for the newly assigned buffer slot
+    metainfo[bufferNum].free      = false;
+    metainfo[bufferNum].dirty     = false;
+    metainfo[bufferNum].blockNum  = blockNum;
+    metainfo[bufferNum].timeStamp = 0;
+
+    return bufferNum;
 }
 
 int StaticBuffer::getBufferNum(int blockNum) {
@@ -58,3 +99,24 @@ int StaticBuffer::getBufferNum(int blockNum) {
   // 3. If we finish the loop and didn't find it, tell the caller it's missing.
   return E_BLOCKNOTINBUFFER;
 }
+
+int StaticBuffer::setDirtyBit(int blockNum) {
+
+    // Step 1: find which buffer slot holds this block
+    int bufferNum = StaticBuffer::getBufferNum(blockNum);
+
+    // Step 2: handle error cases from getBufferNum()
+    if (bufferNum == E_BLOCKNOTINBUFFER) {
+        return E_BLOCKNOTINBUFFER;
+    }
+
+    if (bufferNum == E_OUTOFBOUND) {
+        return E_OUTOFBOUND;
+    }
+
+    // Step 3: set the dirty bit
+    metainfo[bufferNum].dirty = true;
+
+    return SUCCESS;
+}
+
--- mynitcbase/Schema/Schema.cpp
+++ mynitcbase/Schema/Schema.cpp
@@ -27,3 +27,46 @@ int Schema::closeRel(char relName[ATTR_SIZE]) {
 
     return OpenRelTable::closeRel(relId);
 }
+
+int Schema::renameRel(char oldRelName[ATTR_SIZE], char newRelName[ATTR_SIZE]) {
+
+    // Step 1: block renaming of catalog relations
+    if (strcmp(oldRelName, RELCAT_RELNAME) == 0 || 
+        strcmp(oldRelName, ATTRCAT_RELNAME) == 0 ||
+        strcmp(newRelName, RELCAT_RELNAME) == 0 ||
+        strcmp(newRelName, ATTRCAT_RELNAME) == 0) {
+        return E_NOTPERMITTED;
+    }
+
+    // Step 2: check if the relation is currently open
+    // getRelId returns E_RELNOTOPEN if relation is NOT open
+    // we want to PREVENT renaming if it IS open
+    if (OpenRelTable::getRelId(oldRelName) != E_RELNOTOPEN) {
+        return E_RELOPEN;
+    }
+
+    // Step 3: delegate to Block Access Layer
+    int retVal = BlockAccess::renameRelation(oldRelName, newRelName);
+
+    return retVal;
+}
+
+int Schema::renameAttr(char relName[ATTR_SIZE], char oldAttrName[ATTR_SIZE], 
+                       char newAttrName[ATTR_SIZE]) {
+
+    // Step 1: block renaming attributes of catalog relations
+    if (strcmp(relName, RELCAT_RELNAME) == 0 || 
+        strcmp(relName, ATTRCAT_RELNAME) == 0) {
+        return E_NOTPERMITTED;
+    }
+
+    // Step 2: check if the relation is currently open
+    if (OpenRelTable::getRelId(relName) != E_RELNOTOPEN) {
+        return E_RELOPEN;
+    }
+
+    // Step 3: delegate to Block Access Layer
+    int retVal = BlockAccess::renameAttribute(relName, oldAttrName, newAttrName);
+
+    return retVal;
+}
